f71e4eae83681f5fd3a854dd8209c2c4e652067d
Dewick <153211988+biodio332@users.noreply.github.com>
2024-11-07 16:36:58 +0800

    Delete supercook/supercook directory

--- supercook/supercook/src/main/java/projectappdev/supercook/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package projectappdev.supercook.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import projectappdev.supercook.entity.AdminEntity;
-
-@Repository
-public interface AdminRepository extends JpaRepository<AdminEntity, String> {
-}
