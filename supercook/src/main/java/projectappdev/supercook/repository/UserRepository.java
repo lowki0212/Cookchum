7bead6712ae790c012c599476a4aa99c08a57532
polbluu <pauldavebinoya@gmail.com>
2024-11-14 03:56:32 +0800

    Delete supercook directory

--- supercook/src/main/java/projectappdev/supercook/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package projectappdev.supercook.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import projectappdev.supercook.entity.UserEntity;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    Optional<UserEntity> findByEmail(String email);
-}
